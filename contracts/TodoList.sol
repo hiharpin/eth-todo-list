5de8037d02ef055be5d5429b82add708326b8508
hiharpin <23521086@std.stei.itb.ac.id>
2022-11-04 16:27:09 +0700

    add some coding

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,22 @@ pragma solidity ^0.8.17;
 
 contract TodoList {
   uint public taskCount = 0;
+
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+
+ mapping(uint => Task) public tasks;
+
+  constructor() public {
+    createTask("Check out dappuniversity.com");
+  }
+
+  function createTask(string memory _content) public {
+    taskCount ++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+  }
+
 }
